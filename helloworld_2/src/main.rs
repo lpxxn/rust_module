d12f1e9edd5cb6882f57a4705551834b65d754d1
Aiden <mi_duo@live.com>
2022-06-18 11:27:02 +0800

    feat: rust

--- helloworld_2/src/main.rs
+++ helloworld_2/src/main.rs
@@ -1,8 +1,15 @@
 mod user_info;
 use user_info::user::User;
 
+mod student;
+use student::user::User as User2;
+
 fn main() {
     let u1 = User::new_user(String::from("tom"), 5);
     println!("user name: {}", u1.name());
     println!("1+2: {}", user_info::user::add(1, 2));
+    
+    println!("{:?}", User2::new_user("hello".to_string(), 111));
+
+    student::abc::hello();
 }
